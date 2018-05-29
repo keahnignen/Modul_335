07e8d47a6eecf759506fdf0b87ce127fee111d79
Kenan <kingkoshtnex@gmail.com>
2018-05-29 11:00:31 +0200

    Fuck me

--- BadiApp/feature/src/main/java/fg/badoapp/feature/MainActivity.java
+++ BadiApp/feature/src/main/java/fg/badoapp/feature/MainActivity.java
@@ -17,11 +17,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-<<<<<<< HEAD
-
-=======
         setTitle("Overwatch");
->>>>>>> 8854d072e62283ff29c51eb706f10235daa22508
+
     }
 
 
