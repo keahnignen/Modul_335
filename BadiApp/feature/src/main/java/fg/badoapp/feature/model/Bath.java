52f4387fef7f1c1959966fc2ff1db1d946edbd10
Kenan <kingkoshtnex@gmail.com>
2018-05-29 10:02:49 +0200

    Penis

--- BadiApp/feature/src/main/java/fg/badoapp/feature/model/Bath.java
+++ BadiApp/feature/src/main/java/fg/badoapp/feature/model/Bath.java
@@ -4,10 +4,44 @@ import java.util.List;
 
 public class Bath {
 
-    public int id;
-    public String name;
-    public List<Pool> Pools;
-    public int Kanton;
+    private int id;
+    private String name;
+    private List<Pool> pools;
+    private int kanton;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Pool> getPools() {
+        return pools;
+    }
+
+    public void setPools(List<Pool> pools) {
+        this.pools = pools;
+    }
+
+    public int getKanton() {
+        return kanton;
+    }
+
+    public void setKanton(int kanton) {
+        this.kanton = kanton;
+    }
+
+
 
 
 }
