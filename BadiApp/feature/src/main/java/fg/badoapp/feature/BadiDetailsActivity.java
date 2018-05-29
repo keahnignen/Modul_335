a2143ed9b3869b8d52f915a2b6df83b1ffcf84d6
Kenan <kingkoshtnex@gmail.com>
2018-05-29 11:13:19 +0200

    Du nennst mich Spitzkopf

--- BadiApp/feature/src/main/java/fg/badoapp/feature/BadiDetailsActivity.java
+++ BadiApp/feature/src/main/java/fg/badoapp/feature/BadiDetailsActivity.java
@@ -2,8 +2,14 @@ package fg.badoapp.feature;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 public class BadiDetailsActivity extends AppCompatActivity {
+
+    private int bathId;
+    private ProgressBar progressBar;
+
+    private final static String WIE_WARM_API_URL = "http://www.wiewarm.ch/api/v1/bad.json/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
