8854d072e62283ff29c51eb706f10235daa22508
Kenan <kingkoshtnex@gmail.com>
2018-05-29 10:55:47 +0200

    SuiCide

--- BadiApp/feature/src/main/java/fg/badoapp/feature/MainActivity.java
+++ BadiApp/feature/src/main/java/fg/badoapp/feature/MainActivity.java
@@ -1,7 +1,15 @@
 package fg.badoapp.feature;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import fg.badoapp.feature.dal.BadiDao;
+import fg.badoapp.feature.model.Bath;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +19,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setTitle("Overwatch");
     }
+
+
+    private void addBadisToList()
+    {
+        ListView badis = findViewById(R.id.badiliste);
+        ArrayAdapter<Bath> badiAdapter =
+                new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1);
+        badiAdapter.addAll(BadiDao.getAll());
+        badis.setAdapter(badiAdapter);
+
+        AdapterView.OnItemClickListener mListClickedHandler = new
+                AdapterView.OnItemClickListener()
+                {
+                    public void onItemClick(AdapterView parent, View v, int position, long id)
+                    {
+                        Intent intent = new Intent(getApplicationContext(), BadiDetailsActivity.class);
+                        Bath selected = (Bath)parent.getItemAtPosition(position);
+                        intent.putExtra("badiId", selected.getId());
+                        intent.putExtra("badiName", selected.getName());
+                        startActivity(intent);
+                    }
+                };
+        badis.setOnItemClickListener(mListClickedHandler);
+
+    }
+
 }
