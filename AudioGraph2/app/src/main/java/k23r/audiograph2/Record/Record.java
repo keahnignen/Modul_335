447c3f99d60f2b88449fe6e2e74d2579f151d5ed
Kenan <kingkoshtnex@gmail.com>
2018-05-30 14:09:13 +0200

    Sbuete schwanz

--- AudioGraph2/app/src/main/java/k23r/audiograph2/Record/Record.java
+++ AudioGraph2/app/src/main/java/k23r/audiograph2/Record/Record.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Date;
+
 import be.tarsos.dsp.AudioDispatcher;
 import be.tarsos.dsp.AudioEvent;
 import be.tarsos.dsp.AudioProcessor;
@@ -16,11 +18,13 @@ import k23r.audiograph2.R;
 public class Record {
 
     private Activity a;
+    private Recordation rec;
 
     public Record(Activity a)
     {
         this.a = a;
         AudioPermission ap = new AudioPermission(a, this);
+        rec = new Recordation();
     }
 
 
@@ -50,33 +54,18 @@ public class Record {
 
     }
 
+
+    Date d = new Date();
+
     private void processPitch(float pitchInHz) {
         TextView pitchText = (TextView) a.findViewById(R.id.lblFrequency);
         TextView noteText = (TextView) a.findViewById(R.id.lblNote);
 
         pitchText.setText("" + pitchInHz);
-
-        if (pitchInHz >= 110 && pitchInHz < 123.47) {
-            //A
-            noteText.setText("A");
-        } else if (pitchInHz >= 123.47 && pitchInHz < 130.81) {
-            //B
-            noteText.setText("B");
-        } else if (pitchInHz >= 130.81 && pitchInHz < 146.83) {
-            //C
-            noteText.setText("C");
-        } else if (pitchInHz >= 146.83 && pitchInHz < 164.81) {
-            //D
-            noteText.setText("D");
-        } else if (pitchInHz >= 164.81 && pitchInHz <= 174.61) {
-            //E
-            noteText.setText("E");
-        } else if (pitchInHz >= 174.61 && pitchInHz < 185) {
-            //F
-            noteText.setText("F");
-        } else if (pitchInHz >= 185 && pitchInHz < 196) {
-            //G
-            noteText.setText("G");
-        }
+        Date d = new Date();
+        rec.swag.put(d.toString(), pitchInHz);
     }
+
+
+
 }
