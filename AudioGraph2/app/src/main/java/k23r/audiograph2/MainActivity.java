74b9f3dd963407c00e7583155197557a52eacfb1
Kenan <kingkoshtnex@gmail.com>
2018-05-30 09:17:40 +0200

    created dick, with older version of preskin

--- AudioGraph2/app/src/main/java/k23r/audiograph2/MainActivity.java
+++ AudioGraph2/app/src/main/java/k23r/audiograph2/MainActivity.java
@@ -1,15 +1,9 @@
-package kt.audiograph;
-
-/**
- * @msg I like dicks in my butthole
- */
+package k23r.audiograph2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.TextView;
 
 import be.tarsos.dsp.AudioDispatcher;
@@ -20,43 +14,21 @@ import be.tarsos.dsp.pitch.PitchDetectionHandler;
 import be.tarsos.dsp.pitch.PitchDetectionResult;
 import be.tarsos.dsp.pitch.PitchProcessor;
 
-
-public class Record extends AppCompatActivity implements View.OnClickListener
-{
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_record);
-        addBadisToList();
+        setContentView(R.layout.activity_main);
         StraitCopyied();
     }
 
-    @Override
-    public void onClick(View v) {
 
-    }
-
-    public void addBadisToList()
-    {
-
-        AdapterView.OnClickListener mListClickedHandler = new
-                View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-
-                    }
-                };
-        findViewById(R.id.btnSwag).setOnClickListener(this);
-
-    }
-
-    public void StraitCopyied()
-    {
+    public void StraitCopyied() {
         AudioDispatcher dispatcher =
-                AudioDispatcherFactory.fromDefaultMicrophone(22050,1024,0);
+                AudioDispatcherFactory.fromDefaultMicrophone(22050, 1024, 0);
         PitchDetectionHandler pdh = new PitchDetectionHandler() {
             @Override
-            public void handlePitch(PitchDetectionResult res, AudioEvent e){
+            public void handlePitch(PitchDetectionResult res, AudioEvent e) {
                 final float pitchInHz = res.getPitch();
                 runOnUiThread(new Runnable() {
                     @Override
@@ -74,39 +46,32 @@ public class Record extends AppCompatActivity implements View.OnClickListener
     }
 
     public void processPitch(float pitchInHz) {
-        TextView pitchText =  (TextView) findViewById(R.id.textSwag);
-        TextView noteText = (TextView) findViewById(R.id.textSwag2);
+        TextView pitchText = (TextView) findViewById(R.id.lblFrequency);
+        TextView noteText = (TextView) findViewById(R.id.lblNote);
 
         pitchText.setText("" + pitchInHz);
 
-        if(pitchInHz >= 110 && pitchInHz < 123.47) {
+        if (pitchInHz >= 110 && pitchInHz < 123.47) {
             //A
             noteText.setText("A");
-        }
-        else if(pitchInHz >= 123.47 && pitchInHz < 130.81) {
+        } else if (pitchInHz >= 123.47 && pitchInHz < 130.81) {
             //B
             noteText.setText("B");
-        }
-        else if(pitchInHz >= 130.81 && pitchInHz < 146.83) {
+        } else if (pitchInHz >= 130.81 && pitchInHz < 146.83) {
             //C
             noteText.setText("C");
-        }
-        else if(pitchInHz >= 146.83 && pitchInHz < 164.81) {
+        } else if (pitchInHz >= 146.83 && pitchInHz < 164.81) {
             //D
             noteText.setText("D");
-        }
-        else if(pitchInHz >= 164.81 && pitchInHz <= 174.61) {
+        } else if (pitchInHz >= 164.81 && pitchInHz <= 174.61) {
             //E
             noteText.setText("E");
-        }
-        else if(pitchInHz >= 174.61 && pitchInHz < 185) {
+        } else if (pitchInHz >= 174.61 && pitchInHz < 185) {
             //F
             noteText.setText("F");
-        }
-        else if(pitchInHz >= 185 && pitchInHz < 196) {
+        } else if (pitchInHz >= 185 && pitchInHz < 196) {
             //G
             noteText.setText("G");
         }
     }
-
 }
