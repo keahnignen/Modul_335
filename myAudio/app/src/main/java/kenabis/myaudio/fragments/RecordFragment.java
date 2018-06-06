1e3545f8f6f041a0c27b07e4a0092440b0cfdd0e
Kenan <kingkoshtnex@gmail.com>
2018-06-06 12:54:33 +0200

    tbio is swiss and europain white trash, even worse is his ugly moutache,

--- myAudio/app/src/main/java/kenabis/myaudio/fragments/RecordFragment.java
+++ myAudio/app/src/main/java/kenabis/myaudio/fragments/RecordFragment.java
@@ -1,7 +1,10 @@
 package kenabis.myaudio.fragments;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -9,13 +12,26 @@ import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
+
+import com.jjoe64.graphview.DefaultLabelFormatter;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import kenabis.myaudio.R;
 import kenabis.myaudio.record.audio.AudioRecorder;
+import kenabis.myaudio.record.graph.StaticGraph;
 
 public class RecordFragment extends Fragment
 {
+    private boolean createdAndFirstTime = false;
     private String name = "Record";
     private AudioRecorder recorder;
 
@@ -30,6 +46,32 @@ public class RecordFragment extends Fragment
         this.requestAudioPermissions();
 
         return(rootView);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        final Button button = getView().findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), StaticGraph.class);
+                intent.putExtra("array", toArray(pointsForStore));
+                startActivity(intent);
+            }
+        });
+    }
+
+    private float[] toArray(List<Float> swag)
+    {
+        float[] floats = new float[swag.size()];
+        int i = 0;
+
+        for (float f : swag)
+        {
+            floats[i] = f;
+            i++;
+        }
+        return floats;
     }
 
     public String getName()
@@ -96,6 +138,7 @@ public class RecordFragment extends Fragment
             if (this.recorder.isRecording)
             {
                 this.recorder.stopRecording();
+
             }
             else
             {
@@ -103,6 +146,71 @@ public class RecordFragment extends Fragment
             }
         }
     }
+
+    private LineGraphSeries<DataPoint> liveGraphSeries = new LineGraphSeries<DataPoint>();
+    private List<Float> pointsForStore = new ArrayList<Float>();
+
+
+    private void setGraph()
+    {
+        GraphView graphView = (GraphView) getView().findViewById(R.id.graph);
+
+        /*
+        for (FrequencyPoint fp: rec.pointsForStore) {
+            liveGraphSeries.appendData(new DataPoint(fp.date, fp.frequency), true, rec.pointsForStore.size(), true);
+        }
+        */
+
+        /*
+        graphView.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter() {
+
+            @Override
+            public String formatLabel(double value, boolean isValueX) {
+                if (isValueX) {
+                    // show normal x values
+                    return super.formatLabel(value, isValueX);
+                } else {
+                    // show currency for y values
+                    return super.formatLabel(value, isValueX) + " €";
+                }
+            }
+        });
+        */
+
+
+
+        NumberFormat nf = NumberFormat.getInstance();
+        nf.setMinimumFractionDigits(13);
+        nf.setMinimumIntegerDigits(20);
+
+        graphView.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter(nf, nf));
+
+
+/*
+        StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(graphView);
+        staticLabelsFormatter.setHorizontalLabels(new String[] {"old", "middle", "new"});
+        staticLabelsFormatter.setVerticalLabels(new String[] {"low", "middle", "high"});
+        graphView.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);
+
+*/
+        graphView.addSeries(liveGraphSeries);
+    }
+
+    public void addDatapoint(float frequency, boolean b, int i, boolean b1) {
+
+        pointsForStore.add(frequency);
+        liveGraphSeries.appendData(new DataPoint(new Date(), frequency), false, 50, false);
+
+        if (createdAndFirstTime)
+        {
+            createdAndFirstTime = false;
+            liveGraphSeries.setColor(Color.GREEN);
+            setGraph();
+            GraphView graphView = (GraphView) getView().findViewById(R.id.graph);
+            graphView.addSeries(liveGraphSeries);
+        }
+    }
+
 
     //  fragment.addDatapoint(pitchInHz, false, 100, false);
 }
